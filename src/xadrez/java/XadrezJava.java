1ea833b0ade091bf49b7e8919b21b21de98026b9
Luís Otávio <luizcanalgame@gmail.com>
2019-07-25 10:34:41 -0300

    projeto criado

---
+++ src/xadrez/java/XadrezJava.java
@@ -0,0 +1,13 @@
+package xadrez.java;
+
+/**
+ *jogo de xadres do curso de java
+ * @author luizc
+ */
+public class XadrezJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+}
